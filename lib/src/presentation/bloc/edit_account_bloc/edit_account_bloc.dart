03f5e99ea01e7bbd25d71307b3bceb2e8320334a
Sergio Sarmiento <sisarmientob@unal.edu.co>
2021-05-02 20:56:40 -0500

    Se agrega pagina de edicion de cuenta

---
+++ lib/src/presentation/bloc/edit_account_bloc/edit_account_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:kurztrip_ma/src/domain/entities/count/User.dart';
+import 'package:kurztrip_ma/src/presentation/bloc/edit_account_bloc/edit_account_event.dart';
+import 'package:kurztrip_ma/src/presentation/bloc/edit_account_bloc/edit_account_state.dart';
+
+class EditAccountBloc extends Bloc<EditAccountEvent, EditAccountState> {
+  EditAccountBloc(User user)
+      : super(EditShowing(
+            name: user.name,
+            lastname: user.lastName,
+            email: user.email,
+            cellphone: user.cellphone));
+  @override
+  Stream<EditAccountState> mapEventToState(
+    EditAccountEvent event,
+  ) async* {
+    if (state is EditShowing) {
+      EditShowing old = state as EditShowing;
+      if (event is UpdateCellphone) {
+        yield old.copyWith(cellphone: event.cellphone);
+      } else if (event is UpdateEmail) {
+        yield old.copyWith(email: event.email);
+      } else if (event is UpdateLastName) {
+        yield old.copyWith(lastname: event.lastname);
+      } else if (event is UpdateName) {
+        yield old.copyWith(name: event.name);
+      }
+    }
+  }
+}
