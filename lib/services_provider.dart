7948b860a11b4574bfdc03dd59c41d8a95545b6b
juacostar <juacostar@unal.edu.co>
2021-05-02 21:19:53 -0500

    user module completed

--- lib/services_provider.dart
+++ lib/services_provider.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
+import 'package:kurztrip_ma/src/data/count/organization_server_repository.dart';
+import 'package:kurztrip_ma/src/data/count/user_server_repository.dart';
+import 'package:kurztrip_ma/src/data/driver/driver_server_repository.dart';
 import 'package:kurztrip_ma/src/data/truck/truck_server_repository.dart';
+import 'package:kurztrip_ma/src/domain/repositories/locations_repository.dart';
+import 'package:kurztrip_ma/src/domain/repositories/organization_repository.dart';
 import 'package:kurztrip_ma/src/domain/repositories/truck_repository.dart';
 import 'package:kurztrip_ma/src/data/package/package_server_repository.dart';
 import 'package:kurztrip_ma/src/domain/repositories/package_repository.dart';
+import 'package:kurztrip_ma/src/domain/repositories/user_repository.dart';
 import 'package:kurztrip_ma/src/presentation/bloc/first_page_bloc/first_page_bloc.dart';
 import 'package:kurztrip_ma/src/presentation/bloc/homepage/homepage_bloc.dart';
 import 'package:kurztrip_ma/src/presentation/bloc/main_page_bloc/main_page_bloc.dart';
@@ -39,4 +45,7 @@ void registerUserService() {
   //Repositories
   getIt.registerLazySingleton<TruckRepository>(() => TruckServerRepository());
   getIt.registerLazySingleton<PackageRepository>(() => PackageServerRepository());
+  getIt.registerLazySingleton<LocationsRepository>(() => DriverServerRepository());
+  getIt.registerLazySingleton<UserRepository>(() => UserServerRepository());
+  getIt.registerLazySingleton<OrganizationRepository>(() => OrganizationServerRepository());
 }
