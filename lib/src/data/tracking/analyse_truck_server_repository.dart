5a167c306c92d740dad1cfeb6e97ecada2b6e359
Javier Ortiz <jortizsi@unal.edu.co>
2021-05-04 05:43:31 -0500

    GetAllRoutes

--- lib/src/data/tracking/analyse_truck_server_repository.dart
+++ lib/src/data/tracking/analyse_truck_server_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:graphql/client.dart';
 import 'package:kurztrip_ma/src/data/client_config.dart';
 import 'package:kurztrip_ma/src/domain/entities/Tracking/Route.dart';
@@ -14,8 +15,6 @@ class AnalyseTruckServerRepository {
     mutation createAnalyseRouteMA($id: Int!){
       createAnalyseRoute(id: $id){
         id
-        packages
-        route
         fuel
         fuel_capacity
         fuel_type
@@ -42,23 +41,16 @@ class AnalyseTruckServerRepository {
   */
 
   // @override
-  Future<Route> createAnalyseRoute(int id) async {
-    final MutationOptions options = MutationOptions(
-        document: gql(createAnalyseRouteMA),
-        variables: <String, dynamic>{
-          'id': id,
-        });
+  Future<bool> createAnalyseRoute(int id) async {
+    final MutationOptions options = MutationOptions(document: gql(createAnalyseRouteMA), variables: <String, dynamic>{
+      'id': id,
+    });
     final result = await getGraphQLClient().mutate(options);
     if (result.hasException) {
       throw result.exception;
     }
-    final AT_result = result.data['createRoute'];
-    return Route(
-        id: AT_result['id'],
-        starting_time: AT_result['starting_time'],
-        p_longitudes: AT_result['p_longitudes'],
-        p_latitudes: AT_result['p_latitudes'],
-        driver_long: AT_result['driver_long'],
-        driver_lat: AT_result['driver_lat']);
+    final AT_result = result.data['createAnalyseRoute'];
+    debugPrint(AT_result['state']);
+    return true;
   }
 }
