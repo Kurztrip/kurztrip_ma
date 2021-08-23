504cbf0134b82d5f8f3fe6dad57abfe207f4af06
Javier Ortiz <jortizsi@unal.edu.co>
2021-08-23 13:30:37 -0500

    title y body push

--- lib/Services/push_notifications_service.dart
+++ lib/Services/push_notifications_service.dart
@@ -1,20 +1,38 @@
+import 'dart:async';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class PushNotificationsService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static StreamController<String> _messageStreamController = new StreamController.broadcast();
+
+  static String notification_title = '';
+  static String notification_body = '';
 
   static Future _backgroundHandler(RemoteMessage message) async {
     print('onBackground Handler ${message.messageId}');
+    notification_title = message.notification?.title ?? 'Sin Titulo';
+    notification_body = message.notification?.body ?? 'Sin Cuerpo';
+    print(notification_title);
+    print(notification_body);
   }
 
   static Future _onMessageHandler(RemoteMessage message) async {
     print('onMessageHandler Handler ${message.messageId}');
+    notification_title = message.notification?.title ?? 'Sin Titulo';
+    notification_body = message.notification?.body ?? 'Sin Cuerpo';
+    print(notification_title);
+    print(notification_body);
   }
 
   static Future _onMessageOpenApp(RemoteMessage message) async {
     print('onMessageOpenApp Handler ${message.messageId}');
+    notification_title = message.notification?.title ?? 'Sin Titulo';
+    notification_body = message.notification?.body ?? 'Sin Cuerpo';
+    print(notification_title);
+    print(notification_body);
   }
 
   static Future initializeApp() async {
@@ -31,5 +49,9 @@ class PushNotificationsService {
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
 
     //LOCAL NOTIFICATIONS
+  }
+
+  static closeStreams() {
+    _messageStreamController.close();
   }
 }
